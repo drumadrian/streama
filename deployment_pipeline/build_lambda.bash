cdddf5b59ebf5c8ffd62191843e69525684fb3ce
drumadrian <adrian.drummond@gmail.com>
2019-05-19 23:00:03 -0700

    adding new files

---
+++ deployment_pipeline/build_lambda.bash
@@ -0,0 +1,11 @@
+echo -e "\n\n"
+echo -e "......Building   streama lambda zip file"
+echo -e "\n\n"
+DEPLOYMENTBUCKETKEYPARAMETER=transcoder.zip
+rm $DEPLOYMENTBUCKETKEYPARAMETER
+cd venv/lib/python3.6/site-packages/
+zip -r9 ../../../../function.zip .
+cd ../../../../
+# cp aws-elasticsearch-index-curator.py index.py
+zip -g function.zip index.py
+mv function.zip $DEPLOYMENTBUCKETKEYPARAMETER
